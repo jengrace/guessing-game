080625c84123212686c57d9ddedba212d4a8a6da
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 15:17:59 -0800

    meets all requirements

--- game.py
+++ game.py
@@ -1,2 +1,26 @@
 # Put your code here
-print "greetings"
+import random
+
+print "Greetings!"
+name = raw_input("Please introduce yourself: ")
+print name, "Let's play a game!"
+print name, "I'm thinking of a number between 1 and 100... wanna guess?"
+solution = random.randint(0, 100)
+
+
+def guessing():
+    still_guessing = True
+    num_guesses = 0
+    while still_guessing:
+        user_guess = int(raw_input("Please enter your guess: "))
+        num_guesses += 1
+        if user_guess > solution:
+            print "your guess is too high.  Try again."
+        elif user_guess < solution:
+            print "your guess is too low. Try again"
+        else:
+            print "Yay, you are correct!"
+            print "It took you only %i guesses" % num_guesses
+            still_guessing = False
+
+guessing()
