3b3c32623c93bd90107b8ac015e428199e023ce7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 16:34:01 -0800

    added best score

--- game.py
+++ game.py
@@ -5,22 +5,53 @@ print "Greetings!"
 name = raw_input("Please introduce yourself: ")
 print name, "Let's play a game!"
 print name, "I'm thinking of a number between 1 and 100... wanna guess?"
-solution = random.randint(0, 100)
+
+score_list = []
 
 
-def guessing():
+def pick_number():
+    solution = random.randint(1, 100)
+    print "winning number is: %i" % solution
+    return solution
+
+
+def guessing(solution):
     still_guessing = True
     num_guesses = 0
+
     while still_guessing:
-        user_guess = int(raw_input("Please enter your guess: "))
+        user_guess = raw_input("Please enter your guess: ")
         num_guesses += 1
-        if user_guess > solution:
+        try:
+            user_guess = int(user_guess)
+        except ValueError:
+            print "That's not a number, %s! try again." % name
+            continue
+        if user_guess < 0 or user_guess > 100:
+            print "Follow instructions stupid! Choose number between 1 and 100!"
+        elif user_guess > solution:
             print "your guess is too high.  Try again."
         elif user_guess < solution:
             print "your guess is too low. Try again"
         else:
             print "Yay, you are correct!"
             print "It took you only %i guesses" % num_guesses
-            still_guessing = False
+            score_list.append(num_guesses)
+            replay = raw_input("Would you like to play again? y/n")
+            if replay == "y":
+                main()
+            else:
+                print "Thanks for playing!"
+                still_guessing = False
+                break
+    print score_list
+    print "Congrats! You best score was: ", min(score_list)
 
-guessing()
+
+def main():
+    solution = pick_number()
+    guessing(solution)
+
+
+if __name__ == '__main__':
+    main()
